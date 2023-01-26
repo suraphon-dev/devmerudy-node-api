8dae1617af29afb2817600e8fee7f04150a3113d
suraphon <suraphon.k@merudy.com>
2023-01-26 11:25:53 +0700

    update service account key

--- config.js
+++ config.js
@@ -1,15 +1,27 @@
-const firebase = require('firebase')
-const firebaseConfig = {
-   apiKey: 'AIzaSyDJeXdX55-smGayK9g_UQJ7JhASKz6zljA',
-   authDomain: 'devmerudy-node-e1e45.firebaseapp.com',
-   projectId: 'devmerudy-node-e1e45',
-   storageBucket: 'devmerudy-node-e1e45.appspot.com',
-   messagingSenderId: '339156486912',
-   appId: '1:339156486912:web:40cbc6f886db105cb741e5',
-   measurementId: 'G-EVJRVVN4C2'
-}
+// const firebase = require('firebase')
+// const firebaseConfig = {
+//    apiKey: 'AIzaSyDJeXdX55-smGayK9g_UQJ7JhASKz6zljA',
+//    authDomain: 'devmerudy-node-e1e45.firebaseapp.com',
+//    projectId: 'devmerudy-node-e1e45',
+//    storageBucket: 'devmerudy-node-e1e45.appspot.com',
+//    messagingSenderId: '339156486912',
+//    appId: '1:339156486912:web:40cbc6f886db105cb741e5',
+//    measurementId: 'G-EVJRVVN4C2'
+// }
 
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore()
+// firebase.initializeApp(firebaseConfig)
+// const db = firebase.firestore()
+// const Employee = db.collection('Employee')
+// module.exports = Employee
+
+const admin = require('firebase-admin')
+const serviceAccount = require('./serviceAccountKey.json')
+
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
 const Employee = db.collection('Employee')
 module.exports = Employee
